31702071b96137213d1e48e88332ef8238d25503
Didierrld <53330142+Didierrld@users.noreply.github.com>
2022-03-11 16:22:58 +0100

    Add files via upload

---
+++ cours/PHP - MySQL/CRUD/employees.sql
@@ -0,0 +1,20 @@
+/* creation BDD */ 
+
+create database if not exists demo default character set utf8 collate utf8_general_ci;
+ 
+/* creation de la table */
+
+CREATE TABLE IF NOT EXISTS `employees` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `salary` int(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+/* ajout d'infos */
+
+INSERT INTO `employees` (`id`, `name`, `address`, `salary`) VALUES
+(1, 'Roland Mendel', 'C/ Araquil, 67, Madrid', 5000),
+(2, 'Victoria Ashworth', '35 King George, London', 6500),
+(3, 'Martin Blank', '25, Rue Lauriston, Paris', 8000);
